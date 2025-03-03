d218c914334cf1a149db4494f540e1d3e1b94715
marianaaam <mmarinrojas97@gmail.com>
2025-03-02 21:15:50 -0500

    Implementación de Activitys para registro, recuperación de contraseña y perfil. Validación y redirección para home y registro.

---
+++ app/src/main/java/com/example/lifesync/RecoverPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifesync
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class RecoverPasswordActivity : AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recover_password)
+    }
+}
